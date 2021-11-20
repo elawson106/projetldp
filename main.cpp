1e72bbf168a76534520b91f394f24e70fcc638d6
OpenConnection <amggaming@hotmail.fr>
2021-11-20 16:03:44 +0100

    Partie - Commit Biblio

--- main.cpp
+++ main.cpp
@@ -1,6 +1,366 @@
-//projet ldp candy crush
-//Test push edem 
+
+// These should include everything you might use
+#include <FL/Fl.H>
+#include <FL/fl_draw.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_PNG_Image.H>
+#include <FL/Fl_Box.H>
+#include <string>
+#include <math.h>
+#include <time.h>
+#include <chrono>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <array>
+
+using namespace std;
 
 
-int r = 44;
-int t = 0;
+struct Point {
+  int x, y;
+};
+
+/*--------------------------------------------------
+
+Text class.
+
+Use to display text on the screen. For example:
+
+Text("Hello!", {250, 250}).draw();
+
+will create a text temporary instance with the
+string "Hello!" centered at 250, 250 and will
+call draw on the temporary.
+
+It should have all the features you need and you
+should not need to edit it.
+
+--------------------------------------------------*/
+
+class Text {
+  Point center;
+  string s;
+  int fontSize;
+  Fl_Color color;
+ public:
+  //Constructor
+  Text(string s, Point center, int fontSize = 10, Fl_Color color = FL_BLACK):
+    s{s}, center{center}, fontSize{fontSize}, color{color} {}
+
+  //Draw
+  void draw();
+
+  //Setters and getters
+  string getString() {
+    return s;
+  }
+  void setString(const string &newString) {
+    s = newString;
+  }
+  int getFontSize() {
+    return fontSize;
+  }
+  void setFontSize(int newFontSize) {
+    fontSize = newFontSize;
+  }
+  Point getCenter() {
+    return center;
+  }
+  void setCenter(Point newCenter) {
+    center = newCenter;
+  }
+};
+
+void Text::draw() {
+  fl_color(color);
+  fl_font(FL_HELVETICA, fontSize);
+  int width, height;
+  fl_measure(s.c_str(), width, height, false);
+  fl_draw(s.c_str(), center.x-width/2, center.y-fl_descent()+height/2);
+}
+
+/*--------------------------------------------------
+
+Rectangle class.
+
+Use to display a fillend-in rectangle on the screen
+with different colors for the fill and the border
+
+It should have all the features you need and you
+should not need to edit it.
+
+--------------------------------------------------*/
+
+class Rectangle {
+  Point center;
+  int w, h;
+  Fl_Color fillColor, frameColor;
+ public:
+  Rectangle(Point center, int w, int h,
+            Fl_Color frameColor = FL_BLACK,
+            Fl_Color fillColor = FL_WHITE);
+  void draw();
+  void setFillColor(Fl_Color newFillColor);
+  Fl_Color getFillColor() {
+    return fillColor;
+  }
+  void setFrameColor(Fl_Color newFrameColor);
+  Fl_Color getFrameColor() {
+    return frameColor;
+  }
+  void setWidth(int neww) {
+    w = neww;
+  }
+  void setHeight(int newh) {
+    h = newh;
+  }
+  int getWidth() {
+    return w;
+  }
+  int getHeight() {
+    return h;
+  }
+  bool contains(Point p);
+  Point getCenter() {
+    return center;
+  }
+};
+
+Rectangle::Rectangle(Point center, int w, int h,
+                     Fl_Color frameColor,
+                     Fl_Color fillColor):
+  center{center}, w{w}, h{h}, fillColor{fillColor}, frameColor{frameColor} {}
+
+void Rectangle::draw() {
+  fl_draw_box(FL_FLAT_BOX, center.x-w/2, center.y-h/2, w, h, fillColor);
+  fl_draw_box(FL_BORDER_FRAME, center.x-w/2, center.y-h/2, w, h, frameColor);
+  
+}
+
+void Rectangle::setFillColor(Fl_Color newFillColor) {
+  fillColor = newFillColor;
+}
+
+void Rectangle::setFrameColor(Fl_Color newFrameColor) {
+  frameColor = newFrameColor;
+}
+
+bool Rectangle::contains(Point p) {
+  return p.x>=center.x-w/2 &&
+         p.x<center.x+w/2 &&
+         p.y>=center.y-h/2 &&
+         p.y<center.y+h/2;
+}
+
+
+/*--------------------------------------------------
+
+Cell class.
+
+The Canvas class below will have cells as instance
+vraiables and call the methods of Cell
+--------------------------------------------------*/
+
+
+class Cell {
+  Rectangle r;
+  int color;
+ public:
+  // Constructor
+  Cell(Point center, int w, int h, int color);
+
+  // Methods that draw and handle events
+  void draw();
+  void mouseMove(Point mouseLoc);
+  void mouseClick(Point mouseLoc);
+
+};
+
+Cell::Cell(Point center, int w, int h, int color):
+  r(center, w, h, FL_BLACK, FL_WHITE),
+  color{color}{}
+
+void Cell::draw() {
+  r.draw();
+  switch (color)
+  {
+    case 1:
+      r.setFillColor(FL_RED);
+      break;
+    
+    case 2:
+      r.setFillColor(FL_GREEN);
+      break;
+    
+    case 3:
+      r.setFillColor(FL_BLUE);
+      
+      break;
+    
+    case 4:
+      r.setFillColor(FL_YELLOW);
+      break;
+    
+    case 5:
+      r.setFillColor(fl_rgb_color(255, 165, 0));
+      break;
+    
+    case 6:
+      r.setFillColor(fl_rgb_color(143, 0, 255));
+      break;
+
+    default:
+      break;
+  }
+  
+
+}
+
+void Cell::mouseMove(Point mouseLoc) {
+  if (r.contains(mouseLoc)) {
+    r.setFrameColor(FL_RED);
+  } else {
+    r.setFrameColor(FL_BLACK);
+  }
+}
+
+
+void Cell::mouseClick(Point mouseLoc) {
+}
+
+
+/*--------------------------------------------------
+
+Canvas class.
+
+One instance of the canvas class is made by the
+MainWindow class.
+
+The fltk system via MainWindow calls:
+
+draw 60 times a second
+mouseMove whenever the mouse is moved
+mouseClick whenever the mouse is clicked
+keyPressed whenever a key is pressed
+
+Any drawing code should be called ONLY in draw
+or methods called by draw. If you try to draw
+elsewhere it will probably crash.
+--------------------------------------------------*/
+
+
+class Canvas {
+  vector< vector<Cell> > cells;
+ public:
+  Canvas();
+  void draw();
+  void mouseMove(Point mouseLoc);
+  void mouseClick(Point mouseLoc);
+  void keyPressed(int keyCode);
+};
+
+Canvas::Canvas() {
+  string niveau;
+  ifstream file;
+  file.open("niveaux/1.txt");
+  getline(file, niveau);
+
+  for (int x = 0; x<9; x++) {
+    cells.push_back({});
+  }
+
+  int elem = 0;
+  for (int y = 0; y<9; y++){
+    for (int x = 0; x<9; x++){
+      int carre = niveau[elem] - '0';
+      cells[x].push_back({{70*x+200, 70*y+100}, 60, 60,carre});
+      elem++;
+    }
+  } 
+
+}
+
+
+void Canvas::draw() {
+  for (auto &v: cells)
+    for (auto &c: v)
+      c.draw();
+}
+
+void Canvas::mouseMove(Point mouseLoc) {
+  for (auto &v: cells)
+    for (auto &c: v)
+      c.mouseMove(mouseLoc);
+}
+
+void Canvas::mouseClick(Point mouseLoc) {
+  for (auto &v: cells)
+    for (auto &c: v)
+      for (auto &c: v) c.mouseClick(mouseLoc);
+}
+
+void Canvas::keyPressed(int keyCode) {
+  switch (keyCode) {
+    case 'q':
+      exit(0);
+  }
+}
+
+/*--------------------------------------------------
+
+MainWindow class.
+
+Do not edit!!!!
+
+--------------------------------------------------*/
+
+class MainWindow : public Fl_Window {
+  Canvas canvas;
+ public:
+  MainWindow() : Fl_Window(500, 50, 1000, 1000, "Lab 3") {
+    Fl::add_timeout(1.0/60, Timer_CB, this);
+    resizable(this);
+  }
+  void draw() override {
+    Fl_Window::draw();
+    canvas.draw();
+  }
+  int handle(int event) override {
+    switch (event) {
+      case FL_MOVE:
+        canvas.mouseMove(Point{Fl::event_x(), Fl::event_y()});
+        return 1;
+      case FL_PUSH:
+        canvas.mouseClick(Point{Fl::event_x(), Fl::event_y()});
+        return 1;
+      case FL_KEYDOWN:
+        canvas.keyPressed(Fl::event_key());
+        return 1;
+    }
+    return 0;
+  }
+  static void Timer_CB(void *userdata) {
+    MainWindow *o = (MainWindow*) userdata;
+    o->redraw();
+    Fl::repeat_timeout(1.0/60, Timer_CB, userdata);
+  }
+};
+
+
+/*--------------------------------------------------
+
+main
+
+Do not edit!!!!
+
+--------------------------------------------------*/
+
+
+int main(int argc, char *argv[]) {
+  srand(time(0));
+  MainWindow window;
+  window.show(argc, argv);
+  return Fl::run();
+}
